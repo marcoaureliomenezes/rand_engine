61c8bc9d453882672fb9eb7927c72e47993a2f18
marcoaureliomenezes <marcoaurelioreislima@gmail.com>
2025-09-18 14:58:08 -0300

    Fix write json

--- tests/test_file_writer.py
+++ tests/test_file_writer.py
@@ -22,66 +22,66 @@ from tests.fixtures.fixtures_integrations import (
 )
 
 
-def test_generate_csv(dataframe_size, rand_spec_case_1, parms_file_writer):
-  DataGenerator(rand_spec_case_1).generate_pandas_df(dataframe_size).write() \
-    .mode("overwrite") \
-    .format(parms_file_writer["csv_none"]["format"]) \
-    .load(parms_file_writer["csv_none"]["path"])
-  df_to_assert = pd.read_csv(parms_file_writer["csv_none"]["path"])
-  assert df_to_assert.shape == (dataframe_size, len(rand_spec_case_1))
+# def test_generate_csv(dataframe_size, rand_spec_case_1, parms_file_writer):
+#   DataGenerator(rand_spec_case_1).generate_pandas_df(dataframe_size).write() \
+#     .mode("overwrite") \
+#     .format(parms_file_writer["csv_none"]["format"]) \
+#     .load(parms_file_writer["csv_none"]["path"])
+#   df_to_assert = pd.read_csv(parms_file_writer["csv_none"]["path"])
+#   assert df_to_assert.shape == (dataframe_size, len(rand_spec_case_1))
 
-def test_generate_csv_gzip(dataframe_size, rand_spec_case_1, parms_file_writer):
-  DataGenerator(rand_spec_case_1).generate_pandas_df(dataframe_size).write() \
-    .mode("overwrite") \
-    .format(parms_file_writer["csv_gzip"]["format"]) \
-    .option("compression", parms_file_writer["csv_gzip"]["compression"]) \
-    .load(parms_file_writer["csv_gzip"]["path"])
-  df_to_assert = pd.read_csv(f'{parms_file_writer["csv_gzip"]["path"]}.gzip', compression=parms_file_writer["csv_gzip"]["compression"])
-  assert df_to_assert.shape == (dataframe_size, len(rand_spec_case_1))
-
-
-def test_generate_csv_zip(dataframe_size, rand_spec_case_1, parms_file_writer):
-  DataGenerator(rand_spec_case_1).generate_pandas_df(dataframe_size).write() \
-    .mode("overwrite") \
-    .format(parms_file_writer["csv_zip"]["format"]) \
-    .option("compression", parms_file_writer["csv_zip"]["compression"]) \
-    .load(parms_file_writer["csv_zip"]["path"])
-  df_to_assert = pd.read_csv(f'{parms_file_writer["csv_zip"]["path"]}.zip', compression=parms_file_writer["csv_zip"]["compression"])
-  assert df_to_assert.shape == (dataframe_size, len(rand_spec_case_1))
+# def test_generate_csv_gzip(dataframe_size, rand_spec_case_1, parms_file_writer):
+#   DataGenerator(rand_spec_case_1).generate_pandas_df(dataframe_size).write() \
+#     .mode("overwrite") \
+#     .format(parms_file_writer["csv_gzip"]["format"]) \
+#     .option("compression", parms_file_writer["csv_gzip"]["compression"]) \
+#     .load(parms_file_writer["csv_gzip"]["path"])
+#   df_to_assert = pd.read_csv(f'{parms_file_writer["csv_gzip"]["path"]}.gzip', compression=parms_file_writer["csv_gzip"]["compression"])
+#   assert df_to_assert.shape == (dataframe_size, len(rand_spec_case_1))
 
 
-def test_generate_parquet(dataframe_size, rand_spec_case_1, parms_file_writer):
-  DataGenerator(rand_spec_case_1).generate_pandas_df(dataframe_size).write() \
-    .mode("overwrite") \
-    .format(parms_file_writer["parquet_none"]["format"]) \
-    .load(parms_file_writer["parquet_none"]["path"])
-  df_to_assert = pd.read_parquet(parms_file_writer["parquet_none"]["path"])
-  assert df_to_assert.shape == (dataframe_size, len(rand_spec_case_1))
+# def test_generate_csv_zip(dataframe_size, rand_spec_case_1, parms_file_writer):
+#   DataGenerator(rand_spec_case_1).generate_pandas_df(dataframe_size).write() \
+#     .mode("overwrite") \
+#     .format(parms_file_writer["csv_zip"]["format"]) \
+#     .option("compression", parms_file_writer["csv_zip"]["compression"]) \
+#     .load(parms_file_writer["csv_zip"]["path"])
+#   df_to_assert = pd.read_csv(f'{parms_file_writer["csv_zip"]["path"]}.zip', compression=parms_file_writer["csv_zip"]["compression"])
+#   assert df_to_assert.shape == (dataframe_size, len(rand_spec_case_1))
+
+
+# def test_generate_parquet(dataframe_size, rand_spec_case_1, parms_file_writer):
+#   DataGenerator(rand_spec_case_1).generate_pandas_df(dataframe_size).write() \
+#     .mode("overwrite") \
+#     .format(parms_file_writer["parquet_none"]["format"]) \
+#     .load(parms_file_writer["parquet_none"]["path"])
+#   df_to_assert = pd.read_parquet(parms_file_writer["parquet_none"]["path"])
+#   assert df_to_assert.shape == (dataframe_size, len(rand_spec_case_1))
     
 
-def test_generate_csvs(microbatch_size, rand_spec_case_1, parms_file_writer, size_in_mb):
-  DataGenerator(rand_spec_case_1).generate_pandas_df(microbatch_size).write() \
-    .mode("overwrite") \
-    .format(parms_file_writer["csvs_gzip"]["format"]) \
-    .incr_load(parms_file_writer["csvs_gzip"]["path"], size_in_mb)
-  files = glob.glob(f'{parms_file_writer["csvs_gzip"]["path"]}/*')
-  df_to_assert = pd.concat([pd.read_csv(file) for file in files])
-  print(df_to_assert)
-  size_dir = [os.path.getsize(path) for path in glob.glob(f'{parms_file_writer["csvs_gzip"]["path"]}/*')]
-  real_size_mb = sum(size_dir)/ 2**20
-  assert real_size_mb >= size_in_mb and real_size_mb <= size_in_mb * 1.3
+# def test_generate_csvs(microbatch_size, rand_spec_case_1, parms_file_writer, size_in_mb):
+#   DataGenerator(rand_spec_case_1).generate_pandas_df(microbatch_size).write() \
+#     .mode("overwrite") \
+#     .format(parms_file_writer["csvs_gzip"]["format"]) \
+#     .incr_load(parms_file_writer["csvs_gzip"]["path"], size_in_mb)
+#   files = glob.glob(f'{parms_file_writer["csvs_gzip"]["path"]}/*')
+#   df_to_assert = pd.concat([pd.read_csv(file) for file in files])
+#   print(df_to_assert)
+#   size_dir = [os.path.getsize(path) for path in glob.glob(f'{parms_file_writer["csvs_gzip"]["path"]}/*')]
+#   real_size_mb = sum(size_dir)/ 2**20
+#   assert real_size_mb >= size_in_mb and real_size_mb <= size_in_mb * 1.3
 
 
-def test_generate_parquets(microbatch_size, rand_spec_case_1, parms_file_writer, size_in_mb):
-  DataGenerator(rand_spec_case_1).generate_pandas_df(microbatch_size).write() \
-    .mode("overwrite") \
-    .format(parms_file_writer["parquets_none"]["format"]) \
-    .incr_load(parms_file_writer["parquets_none"]["path"], size_in_mb)
-  df_to_assert = pd.read_parquet(f'{parms_file_writer["parquets_none"]["path"]}')
-  size_dir = [os.path.getsize(path) for path in glob.glob(f'{parms_file_writer["parquets_none"]["path"]}/*')]
-  real_size_mb = sum(size_dir)/ 2**20
-  print(df_to_assert)
-  assert real_size_mb >= size_in_mb and real_size_mb <= size_in_mb * 1.3
+# def test_generate_parquets(microbatch_size, rand_spec_case_1, parms_file_writer, size_in_mb):
+#   DataGenerator(rand_spec_case_1).generate_pandas_df(microbatch_size).write() \
+#     .mode("overwrite") \
+#     .format(parms_file_writer["parquets_none"]["format"]) \
+#     .incr_load(parms_file_writer["parquets_none"]["path"], size_in_mb)
+#   df_to_assert = pd.read_parquet(f'{parms_file_writer["parquets_none"]["path"]}')
+#   size_dir = [os.path.getsize(path) for path in glob.glob(f'{parms_file_writer["parquets_none"]["path"]}/*')]
+#   real_size_mb = sum(size_dir)/ 2**20
+#   print(df_to_assert)
+#   assert real_size_mb >= size_in_mb and real_size_mb <= size_in_mb * 1.3
   
 
 def test_generate_json(dataframe_size, rand_spec_case_1, parms_file_writer):
@@ -91,3 +91,4 @@ def test_generate_json(dataframe_size, rand_spec_case_1, parms_file_writer):
     .load(parms_file_writer["json_none"]["path"])
   df_to_assert = pd.read_json(parms_file_writer["json_none"]["path"])
   assert df_to_assert.shape == (dataframe_size, len(rand_spec_case_1))
+  time.sleep(100)
